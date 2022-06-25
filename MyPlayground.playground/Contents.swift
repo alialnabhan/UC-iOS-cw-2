76358fc3f191db232ac344f02d6afbe4f6890dea
alialnabhan <108013363+alialnabhan@users.noreply.github.com>
2022-06-25 19:33:42 +0300

    cw1

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+var favouriteNumber :Int
+var name :String
+var hobby :String
+favouriteNumber = 7
+name = "Ali"
+hobby = "football"
+print("السلام عليكم\(name) الرقم المفضل هو \(favouriteNumber) و هوايتي هي \(hobby)")
+let birthOfDate : Int
+birthOfDate = 1997
+print("السلام عليكم\(name) الرقم المفضل هو \(favouriteNumber) و هوايتي هي \(hobby) \n I was born in \(birthOfDate)")
